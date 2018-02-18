fa4138518e5453d5b790c9d9502df73667832295
Diego Vicente <mail@diego.codes>
2018-02-18 18:11:07 +0100

    Rename rhyme.py to phonetic.py, add consonant rhymes
    Renaming the file for better nomenclature (probably still going to
    change again) and add functions to manage consonant rhymes. Still
    to fix problems with complex words to find adequate suffixes.

--- src/phonetic.py
+++ src/phonetic.py
@@ -7,7 +7,7 @@ nltk.download('cmudict')
 pronounciation = dict(nltk.corpus.cmudict.entries())
 
 
-def get_perfect_rhyme(word):
+def get_perfect_sounds(word):
     """Get the phonetic representation of the syllables after the stress.
 
     :param word: String containing the word.
@@ -16,13 +16,14 @@ def get_perfect_rhyme(word):
 
     """
     try:
-        ending = dropwhile(lambda x: '1' not in x, pronounciation[word])
+        key = word.strip().lower()
+        ending = dropwhile(lambda x: '1' not in x, pronounciation[key])
         return list(ending)
     except KeyError:
         return []
 
 
-def get_vowel_rhyme(word):
+def get_vowel_sounds(word):
     """Get the phonetic representation of the vowels after the stress.
 
     :param word: String containing the word.
@@ -31,7 +32,24 @@ def get_vowel_rhyme(word):
 
     """
     try:
-        ending = dropwhile(lambda x: '1' not in x, pronounciation[word])
+        key = word.strip().lower()
+        ending = dropwhile(lambda x: '1' not in x, pronounciation[key])
         return [s for s in ending if any(char.isdigit() for char in s)]
+    except KeyError:
+        return []
+
+
+def get_consonant_sounds(word):
+    """Get the phonetic representation of the consonants after the stress.
+
+    :param word: String containing the word.
+    :returns: Consonant syllables corresponding to the word.
+    :rtype: list
+
+    """
+    try:
+        key = word.strip().lower()
+        ending = dropwhile(lambda x: '1' not in x, pronounciation[key])
+        return [s for s in ending if not any(char.isdigit() for char in s)]
     except KeyError:
         return []
