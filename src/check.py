dac61201addcdf21bee2a9a889eae74a68a8cb41
Diego Vicente <mail@diego.codes>
2018-02-20 17:30:28 +0100

    More general version of check.py
    Refactor to prevent redundant code. Probably can be improved by
    connecting it to the other module and reducing the shorthands.

--- src/check.py
+++ src/check.py
@@ -1,6 +1,26 @@
 from phonetic import get_consonant_sounds, get_perfect_sounds, get_vowel_sounds
 
 
+def _rhyme(word_a, word_b, sounds_func):
+    """Return whether two words form a rhyme.
+
+    This function is just a general version and the shorthands available in the
+    same module should be used instead, except a custom function for extracting
+    phonemes is to be used.
+
+    :param word_a: first word.
+    :param word_b: second word.
+    :param sounds_func: function to extract relevant phonemes.
+
+    :returns: True if they rhyme following the given conditions.
+
+    :rtype: bool
+    """
+    sounds_a = sounds_func(word_a)
+    sounds_b = sounds_func(word_b)
+    return sounds_a == sounds_b
+
+
 def perfect_rhyme(word_a, word_b):
     """Return whether two words form a perfect rhyme.
 
@@ -10,9 +30,7 @@ def perfect_rhyme(word_a, word_b):
     :rtype: bool
 
     """
-    sounds_a = get_perfect_sounds(word_a)
-    sounds_b = get_perfect_sounds(word_b)
-    return sounds_a == sounds_b
+    return _rhyme(word_a, word_b, get_perfect_sounds)
 
 
 def vowel_rhyme(word_a, word_b):
@@ -20,13 +38,11 @@ def vowel_rhyme(word_a, word_b):
 
     :param word_a: first word.
     :param word_b: second word.
-    :returns: True if they rhyme in vowels.
+    :returns: True if they rhyme on vowels.
     :rtype: bool
 
     """
-    sounds_a = get_vowel_sounds(word_a)
-    sounds_b = get_vowel_sounds(word_b)
-    return sounds_a == sounds_b
+    return _rhyme(word_a, word_b, get_vowel_sounds)
 
 
 def consonant_rhyme(word_a, word_b):
@@ -34,10 +50,8 @@ def consonant_rhyme(word_a, word_b):
 
     :param word_a: first word.
     :param word_b: second word.
-    :returns: True if they rhyme in consonants.
+    :returns: True if they rhyme on consonants.
     :rtype: bool
 
     """
-    sounds_a = get_consonant_sounds(word_a)
-    sounds_b = get_consonant_sounds(word_b)
-    return sounds_a == sounds_b
+    return _rhyme(word_a, word_b, get_consonant_sounds)
